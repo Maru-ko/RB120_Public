11463226c76cc0bbc9778b6aa9d4d04238ae1556
mugenmirai <marcosavila@protonmail.com>
2020-06-08 23:55:35 -0400

    ポップコン噛む音

---
+++ 4 Inheritance/class_machine.rb
@@ -0,0 +1,27 @@
+class Machine
+  @@users = {}
+
+  def initialize(username, password)
+    @username, @password = username, password
+    @@users[username] = password
+    @files = {}
+  end
+
+  def create(filename)
+    time = Time.now
+    @files[filename] = time
+    puts "#{filename} was created by #{@username} at #{time}."
+  end
+
+  def self.get_users
+    @@users.keys
+  end
+end
+
+my_machine = Machine.new("eric", 01234)
+your_machine = Machine.new("you", 56789)
+
+my_machine.create("groceries.txt")
+your_machine.create("todo.txt")
+
+puts "Users: #{Machine.get_users}"
